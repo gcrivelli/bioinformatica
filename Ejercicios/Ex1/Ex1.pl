1c7a20aed6d5146a10adacf5753905deb4957361
Gaston Crivelli <gcrivelli@frba.utn.edu.ar>
2020-12-01 18:58:45 -0300

    ex1 translate

--- Ejercicios/Ex1/Ex1.pl
+++ Ejercicios/Ex1/Ex1.pl
@@ -2,12 +2,14 @@ use warnings;
 use strict;
 use Bio::SeqIO;
 
-my $seqio_obj = Bio::SeqIO->new(-file => "INS-IGF2.gb", -format => "genbank");
+my $seqio_obj = Bio::SeqIO->new(
+    -file => "sequence.gb",
+    -format => "genbank");
 
-open my $handle, '>', 'INS-IGF2.fasta';
+open my $handle, '>', 'sequence.fasta';
 
 while (my $seq_obj = $seqio_obj->next_seq) {
-    print $handle $seq_obj->seq, "\n";
+    print $handle $seq_obj->translate->seq, "\n";
 }
 
 close($handle);
