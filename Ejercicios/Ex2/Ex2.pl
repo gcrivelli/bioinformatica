a31a9218f2f96d16417deeae44674bb2d04f9a09
Gaston Crivelli <gcrivelli@frba.utn.edu.ar>
2020-12-02 10:40:26 -0300

    ordenamos los scrypts por el local y el remoto

--- Ejercicios/Ex2/Ex2.pl
+++ Ejercicios/Ex2/Ex2.pl
@@ -1,42 +1,16 @@
 use strict;
 use warnings;
-use Bio::Tools::Run::RemoteBlast;
 
 my $dir = "inputs";
 opendir DIR, $dir;
 my @dir = readdir(DIR);
 close DIR;
 
-my $remoteBlast = Bio::Tools::Run::RemoteBlast->new(
-    ( "-prog" => "blastp",
-    "-data" => "swissprot",
-    "-expect" => "1e-10",
-    "-readmethod" => "SearchIO" )
-);
+system("makeblastdb", "-in", "swissprot", "-dbtype", "prot", "-out", "resultado.out");
 
 foreach (@dir) {
     if (-f $dir . "/" . $_) {
-
-        my $input = "inputs/" . $_;
-        my $output = "outputs/" . $_ . ".out";
-
-        my $r = $remoteBlast->submit_blast($input);
-
-        while ( my @rids = $remoteBlast->each_rid ) {
-            foreach my $rid ( @rids ) {
-                my $rc = $remoteBlast->retrieve_blast($rid);
-                if( !ref($rc) ) {
-                    if( $rc < 0 ) {
-                        $remoteBlast->remove_rid($rid);
-                    }
-                }
-                else {
-                    my $result = $rc->next_result();
-
-                    $remoteBlast->save_output($output);
-                    $remoteBlast->remove_rid($rid);
-                }
-            }
-        }
+        system("blastp", "-db", "resultado.out"
+            , "-query", "inputs/" . $_, "-out", "outputs/" . $_ . ".out");
     }
 }
